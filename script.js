4512f05803e240ec6d7b1fc364c6f29921241f8e
Marco Cayetano <cayetanomarco@yahoo.com>
2022-02-12 18:00:57 -0800

    add event listeners to buttons

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ let computerSelection;
 let playerSelection;
 let playerScore=0;
 let computerScore=0;
+let resultBox = document.querySelector(".results");
 
 let rockButton = document.querySelector(".rock-btn");
-rockButton.addEventListener("click",() => playRound());
+rockButton.addEventListener("click",() => playRound("rock", computerSelection));
+
+let paperButton = document.querySelector(".paper-btn");
+paperButton.addEventListener("click", () => playRound("paper", computerSelection));
+
+let scissorsButton = document.querySelector(".scissors-btn");
+scissorsButton.addEventListener("click", () => playRound("scissors", computerSelection));
 
 
 
@@ -32,22 +39,21 @@ function playRound(playerSelection, computerSelection){
     switch(true){
         //If both make same selectin - TIE
         case playerSelection===computerSelection:
+            console.log("tie");
             return "It's a tie!";
             break;
         //All possible win moves for PLAYER
         case (playerSelection==='rock'&&computerSelection==='scissors')||(playerSelection==='paper'&&computerSelection==='rock')||(playerSelection==='scissors'&&computerSelection==='paper'):
             playerScore++;
+            console.log("you win!");
             return `You win! ${playerSelection} beats ${computerSelection}`;
             break;
         //All other possibilities are of computer winning
         default:
             computerScore++;
+            console.log("you loose!");
             return `You loose! ${playerSelection} is beaten by ${computerSelection}`;
             break;
 
     }
-}
-
-function game(){
-  
 }
